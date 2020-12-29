d74a026f03d96c0e13204b5fcd469a852c72a42c
kevinacheung <knively@gmail.com>
2020-12-29 13:56:55 -0500

    Testing testing 123

--- basics.py
+++ basics.py
@@ -1,19 +1,19 @@
 import datetime
 mynow = datetime.datetime.now()
-print("The time is " + str(mynow))
+#print("The time is " + str(mynow))
 
 mynumber = 10
 mytext = "Hello"
-print(mytext + " " + str(mynumber))
+#print(mytext + " " + str(mynumber))
 
 x = 15
 y = 2
 z = 1111
-print(x, y, z)
+#print(x, y, z)
 
 studentgrades = [91, 88, 75]
 studsum = sum(studentgrades)
 studlen = len(studentgrades)
-print("The sum is " + str(studsum) + " and the count is " + str(studlen) + " and the average is " + str(studsum/studlen))
+#print("The sum is " + str(studsum) + " and the count is " + str(studlen) + " and the average is " + str(studsum/studlen))
 
 print("Why is Github integration so hard???")
