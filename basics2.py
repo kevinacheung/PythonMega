898b1fc001a15d5b5e10794d82e1411055f4b68b
kevinacheung <knively@gmail.com>
2020-12-29 21:32:14 -0500

    Done for 12/29

---
+++ basics2.py
@@ -0,0 +1,12 @@
+#The Basics: Operations with Data Types
+monday_temperatures = [91, 88, 75]
+monday_temperatures.append(81)
+value = monday_temperatures.index(88)
+print(value)
+print(monday_temperatures.pop())
+
+#more information on indexing
+#positive, negative, slicing with brackets
+#dictionaries do not use indexing, need keys
+
+#The Basics: Functions and Conditionals
