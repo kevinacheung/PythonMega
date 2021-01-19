acb14468947f7b716f57233ee910ecba6ba6485b
kevinacheung <knively@gmail.com>
2021-01-18 22:33:03 -0500

    MLK Day Coding!

--- basics/files.py
+++ basics/files.py
@@ -5,15 +5,26 @@ myfile.close()
 #print(content)
 
 #alternate way that is more streamlined to open and close a file
-with open("texts/fruits.txt") as myfile:
-    content = myfile.read()
+#with open("texts/fruits.txt") as myfile:
+#    content = myfile.read()
 
 #print(content)
 
-with open("texts/vegetables.txt", "w") as myfile:
-    myfile.write("celery\nbroccoli\neggplant\n")
-    myfile.write("spinach")
+#with open("texts/vegetables.txt", "w") as myfile:
+#    myfile.write("celery\nbroccoli\neggplant\n")
+#    myfile.write("spinach")
 
 
 
 # Imported Modules
+import time
+import os
+import pandas
+
+while True:
+    if os.path.exists("texts/temps_today.csv"):
+        data = pandas.read_csv("texts/temps_today.csv")
+        print(data.mean()["st1"])
+    else:
+        print("File doesn't exist")
+    time.sleep(10)
