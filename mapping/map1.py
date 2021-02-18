e059b5791eccfc531ed7e33461dfc6226e844e45
kevinacheung <knively@gmail.com>
2021-02-17 23:08:48 -0800

    Fixing

--- mapping/map1.py
+++
@@ -1,6 +0,0 @@
-#import folium
-#dir(folium)
-#help(folium.map)
-map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles = "Stamen Terrain")
-map.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi Marker", icon=folium.Icon(color='green')))
-map.save("mapping/map1.html")
