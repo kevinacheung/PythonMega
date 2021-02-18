33b732b094566c33caf1237ed3d2b0eaadb325d6
kevinacheung <knively@gmail.com>
2021-02-17 22:20:13 -0800

    Testing

--- mapping/intro.py
+++
@@ -1,5 +0,0 @@
-import folium
-#dir(folium)
-#help(folium.map)
-map = folium.Map(location=[38.58,-99.09], zoom_start=6)
-map.save("map1.html")
---
+++ mapping/map1.py
@@ -0,0 +1,6 @@
+import folium
+#dir(folium)
+#help(folium.map)
+map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles = "Stamen Terrain")
+map.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I'm a Marker", icon=folium.Icon(color='green')))
+map.save("mapping/map1.html")
