2b340288d1f73c0f31fd93d24c2fe2fbf128fbf0
kevinacheung <knively@gmail.com>
2021-02-18 09:12:43 -0800

    Test

---
+++ mapping/mapIntro.py
@@ -0,0 +1,10 @@
+import folium
+# dir(folium)
+# help(folium.map)
+map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Stamen Terrain")
+
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi Marker", icon=folium.Icon(color='green')))
+
+map.add_child(fg)
+map.save("mapping/map1.html")
